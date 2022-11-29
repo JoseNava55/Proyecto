2b53f30044045cd8e0447cd64721a382ab7251f7
JoseNava55 <josenava.g55@gmail.com>
2022-11-29 16:40:30 -0600

    Se agrego la ocion de agregar los cursos y tambien de lo que puede ver el usuario

---
+++ src/models/curso.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const {Schema} = mongoose;
+
+const cursoSchema = new Schema({
+    title:{
+        type:String,
+        require:true,
+        trim: true
+    } ,
+    description:{
+        type: String,
+        require: true
+    },
+    pricing:{
+        type:Number,
+        require: true
+    } 
+})
+
+module.exports= mongoose.model('curso',cursoSchema);
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ import { Router } from "express"
 const router = Router();
 import {renderSignUpForm,signup,renderSigninForm,signin,logout} from "../controllers/user.controller.js";
 const passport= require('passport');
+import Curso from "../models/curso"
+
 
 router.get('/',(req, res, next)=>{
   res.render('index');
@@ -11,8 +13,9 @@ router.get('/about',(req, res, next)=>{
   res.render('about');
 });
 
-router.get('/cursos',isAuthenticated,(req, res, next)=>{
-  res.render('cursos');
+router.get('/cursos',isAuthenticated,async(req, res, next)=>{
+  const cursos = await Curso.find().lean();
+  res.render('cursos',{cursos:cursos});
 });
 
 router.get("/signup",renderSignUpForm);
@@ -29,9 +32,9 @@ router.get('/pusuarios',isAuthenticated,(req, res, next)=>{
   res.render('pusuarios');
 });
 
-router.post('/adminin',(req, res, next)=>{
+router.post('/adminin',async(req, res, next)=>{
   if(req.body.password == "1234" && (req.body.emailad)== "admin@gmail.com"){
-    res.render('adminp');
+    res.redirect('adminp');
   }
   else{
     req.flash("error_msg", "Datos incorrectos");
@@ -39,9 +42,39 @@ router.post('/adminin',(req, res, next)=>{
   }
 });
 
-router.get('/adminin',(req, res, next)=>{
+router.get('/adminin',async(req, res, next)=>{
   res.render('adminin');
 });
+router.get('/adminp',async(req, res, next)=>{
+  const cursos = await Curso.find().lean();
+    res.render('adminp',{cursos:cursos});
+});
+router.post('/adminp',async(req, res, next)=>{
+        const curso =Curso(req.body)
+        const cursoSaved = await curso.save();
+        console.log(cursoSaved);
+        res.redirect('/adminp');
+});
+
+router.get("/edit/:id",async(req,res)=>{
+  //console.log(req.params.id)
+const curso = await Curso.findById(req.params.id).lean()
+
+ // res.render("edit");
+  res.render("edit",{curso});
+});
+
+router.post("/edit/:id",async(req,res)=>{
+  const{id} = req.params
+  await Curso.findByIdAndUpdate(id, req.body)
+  res.redirect('/adminp');
+})
+
+router.get("/delete/:id",async(req,res)=>{
+  const { id } = req.params;
+  await Curso.findByIdAndDelete(id)
+  res.redirect('/adminp');
+})
 
 function isAuthenticated (req, res, next) {
     if (req.isAuthenticated()) {
